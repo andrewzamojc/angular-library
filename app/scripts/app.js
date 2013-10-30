4099b731654249837ca029f0086dab9af05ba834
Andrew Zamojc <andrewzamojc@gmail.com>
2013-10-29 23:50:00 -0400

    Split out app config so we don't include it in built version.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,14 +4,4 @@ angular.module('angularLibraryApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+]);
